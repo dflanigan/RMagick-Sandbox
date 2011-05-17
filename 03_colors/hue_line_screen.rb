09e0986deb12aee64f1e68642109580e4bb1d16f
Dennis Flanigan <dennisf@ucar.edu>
2011-05-17 08:02:28 -0400

    Adding screen image output script

---
+++ 03_colors/hue_line_screen.rb
@@ -0,0 +1,47 @@
+#! /usr/bin/env ruby
+
+require 'RMagick'
+
+def add_a_circle(image_field)
+
+  x_location = rand(image_field.columns)
+
+  y_location = image_field.rows/2
+
+  radius = rand(image_field.rows/30)
+  if radius == 0 
+    radius = 1
+  end
+
+  opacity = rand * 0.25
+
+  width = rand(5)
+  width += 1
+
+  circle = Magick::Draw.new
+  hue = 360 * x_location/image_field.columns
+  color_string = "hsl(#{hue},90,90)"
+  circle.stroke(color_string)
+  circle.fill_opacity(0)
+  circle.stroke_opacity(opacity)
+  circle.stroke_width(width)
+
+  circle.ellipse(x_location,y_location,radius,radius,0,360)
+  circle.draw(image_field)
+end
+
+the_image = Magick::Image.new(1680,1050) {
+  self.background_color = 'black'
+}
+
+500.times do 
+  add_a_circle the_image
+end
+
+the_image.display
+
+the_image.write "circle_hue_line.jpg"
+
+exit
+
+
